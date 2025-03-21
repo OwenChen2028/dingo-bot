c1f709f3c90e689760187d3bd2ed7733779b56ad
Owen Chen <owen.alex.chen@gmail.com>
2025-03-21 13:12:57 -0700

    added read command

---
+++ commands/utility/read.js
@@ -0,0 +1,55 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { Client } = require('pg');
+const dbClient = require('../../dbClient');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('read')
+		.setDescription('Displays the definition of a term from the current server\'s table..')
+        .addStringOption(option =>
+            option.setName('term')
+                .setDescription('The term to find.')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+
+
+	async execute(interaction) {
+        if (!interaction.guild) {
+            return await interaction.reply('This command can only be used in a server.');
+        }
+
+        await interaction.deferReply();
+
+        const term = interaction.options.getString('term');
+
+        const addTermQuery = `
+            SELECT *
+            FROM dingo_${interaction.guild.id}
+            WHERE term = $1;
+        `;
+
+        try {
+            const result = await dbClient.query(addTermQuery, [term]);
+
+            if (result.rows.length === 0) {
+                return await interaction.editReply(`No definition found for **${term}**.`);
+            }
+
+            const { definition, created_by, created_at, modified_by, modified_at, change_count } = result.rows[0];
+            const createdDate = new Date(created_at).toLocaleString();
+            const modifiedDate = modified_at ? new Date(modified_at).toLocaleString() : 'Never';
+
+            const response = `**Term:** ${term}\n**Definition:** ${definition}\n` +
+                `**Created By:** <@${created_by}>\n**Created At:** ${createdDate}\n` +
+                `**Modified By:** ${modified_by ? `<@${modified_by}>` : 'N/A'}\n` +
+                `**Modified At:** ${modifiedDate}\n` +
+                `**Change Count:** ${change_count}`;
+
+            await interaction.editReply(response);
+        }
+        catch (error) {
+            console.error('Error adding term:', error);
+            await interaction.editReply('An error occurred while retrieving the term.');
+        }
+	},
+};
